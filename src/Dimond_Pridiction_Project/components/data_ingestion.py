fc34f21a14d24cd03dc338d7a34af1297957d0ad
priyabghl14 <priyabgl14@gmail.com>
2024-05-07 10:05:54 +0530

    dvc init

--- src/Dimond_Pridiction_Project/components/data_ingestion.py
+++ src/Dimond_Pridiction_Project/components/data_ingestion.py
@@ -27,7 +27,9 @@ class DataIngestion:
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-
+            df_train = pd.DataFrame(train_set)
+            df_test = pd.DataFrame(test_set)
+            print(df.shape,df_train.shape, df_test.shape)
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
